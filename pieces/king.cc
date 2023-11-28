17fc73a15c735d16c66209862d747aabc82d8fab
jbwang <jbwang@uwaterloo.ca>
2023-11-28 00:16:14 -0500

    made some small edits to fields and methods

---
+++ pieces/king.cc
@@ -0,0 +1,11 @@
+#include "king.h"
+#include <vector>
+#include <utility>
+
+using namespace std;
+
+King::King(int weight, Colour side): Piece(weight, side) {}
+
+<vector<pair<char, in>> King::getMoves(Board &b) const{
+	
+}	
--- pieces/king.h
+++ pieces/king.h
@@ -8,17 +8,33 @@ using namespace std;
 class Board;
 
 class King: public Piece, public Observer {
+	// tracks the subjects that the king is observing to make it easier to determine the
+	// legality of a move when the king is in check
 	<vector<Piece*>> subjects;
 	
 public:
-	King(); // ctor
+	King(int weight, Colour side); // ctor
+	~King() override = default; // dtor
 	
-	// returns the subscription of king
+	// Overrides from Piece:	
+	<vector<pair<char, int>> getMoves(Board &b) const override;
+
+	PieceType pieceType() const override;
+
+
+	// Overrides from Observer:
 	Subscription getSubscription const();
 	
-	// adds a
+	void notify(const Subject *item) override;
+
+
+	// Accessors and Mutators
+	// returns a vector of the subjects that the king is observing	
+	<vector<Piece*>> getSubjects() const;
+	
+	// appends a subject to the end of the vector
 	void addSubject(Subject *);
+
+	// removes subject from subjects
 	void dropSubject(Subject *);
-	<vector<pair<char, int>> getMoves(Board &b) const;
-	PieceType pieceType() const;
 };
