f054f9edd889d614e38a2d414d45afed4f24e9da
Richard Wei <richard33113@gmail.com>
2023-11-28 16:27:09 -0500

    changed piece creator to ptr

--- piececreator.cc
+++ piececreator.cc
@@ -1,20 +1,24 @@
 #include "piececreator.h"
+using namespace std;
 
-Piece PieceCreator::createPiece(PieceType type, Colour side, std::pair<char, int> location) {
-	switch (type) {
-		case PieceType::Pawn:
-            return Pawn(side, location);
-		case PieceType::Rook:
-            return Rook(side, location);
-		case PieceType::Knight:
-            return Knight(side, location);
-		case PieceType::Bishop:
-            return Bishop(side, location);
-		case PieceType::Queen:
-            return Queen(side, location);
-		case PieceType::King:
-            return King(side, location);
-		default:
-            return Blank(side, location);
+class PieceCreator {
+public:
+    static unique_ptr<Piece> createPiece(PieceType type, Colour side, pair<char, int> location) {
+        switch (type) {
+            case PieceType::Pawn:
+                return make_unique<Pawn>(side, location);
+            case PieceType::Rook:
+                return make_unique<Rook>(side, location);
+            case PieceType::Knight:
+                return make_unique<Knight>(side, location);
+            case PieceType::Bishop:
+                return make_unique<Bishop>(side, location);
+            case PieceType::Queen:
+                return make_unique<Queen>(side, location);
+            case PieceType::King:
+                return make_unique<King>(side, location);
+            default:
+                return make_unique<Blank>(side, location);
+        }
     }
-}
+};
--- piececreator.h
+++ piececreator.h
@@ -3,7 +3,8 @@
 
 #include <string>
 #include <piece.h>
-#include "none.h"
+#include <memory>
+#include "blank.h"
 #include "pawn.h"
 #include "rook.h"
 #include "knight.h"
@@ -13,7 +14,7 @@
 
 class PieceCreator {
 public:	
-	static Piece createPiece(PieceType type, Colour side, pair<char, int> location);
-}
+	static std::unique_ptr<Piece> createPiece(PieceType type, Colour side, pair<char, int> location);
+};
 
 #endif
