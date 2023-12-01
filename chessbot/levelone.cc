c6b3e151f92d5ee5aec84401af66681c1c26f5da
Jeffrey Xu <55955411+jeffxu2004@users.noreply.github.com>
2023-12-01 23:18:42 +0000

    bot bugfix

--- chessbot/levelone.cc
+++ chessbot/levelone.cc
@@ -11,7 +11,7 @@ public:
 		// getAllMoves does not consider if the move will place/leave the king in check,
 		// as a result we must filter out those moves		
         for (auto move = possibleMoves.begin(); move != possibleMoves.end(); ) {
-            if (kingIsNotCheck(move.first, move.second) {
+            if (b.kingIsNotCheck(move->first, move->second)) {
                 possibleMoves.erase(move);
             } else {
                 move++;
@@ -21,7 +21,7 @@ public:
 		if (!possibleMoves.empty()) {
     	    // Level one bot makes random moves so we use srand and rand
     		// Seed the random number generator
-    		srand(static_cast<unsigned int>(std::time(nullptr)));
+    		srand(static_cast<unsigned int>(time(nullptr)));
 
  	    	// Mod the number by the length of the array of moves
 			// so that the number represents a random index in the list of moves
@@ -30,17 +30,17 @@ public:
 			// If pawn is promoting, pick random piece
 			if (b.isPromoting(possibleMoves[index].first, possibleMoves[index].second)) {
 				int r = rand()%4;
-				if (r == 0) setPromotionPiece(PieceType::Queen);
-				else if (r == 1) setPromotionPiece(PieceType::Rook);
-				else if (r == 2) setPromotionPiece(PieceType::Knight);
-				else if (r == 3) setPromotionPiece(PieceType::Bishop);
+				if (r == 0) b.setPromotionPiece(PieceType::Queen);
+				else if (r == 1) b.setPromotionPiece(PieceType::Rook);
+				else if (r == 2) b.setPromotionPiece(PieceType::Knight);
+				else if (r == 3) b.setPromotionPiece(PieceType::Bishop);
 			}
 
-			playLegalMove(possibleMoves[index].first, possibleMoves[index].second);
+			b.playLegalMove(possibleMoves[index].first, possibleMoves[index].second);
 			return possibleMoves[index];
     	} else {
     	   	// No valid moves, return a '0' instead of a letter from a to h to indicate this 
-        	return {'0', 0};
+        	return {{'0', 0}, {'0', 0}};
 		}
 	}
-}
+};
