8385e9b7fbc65b0decb75645079b2227fc8ac560
Jeffrey Xu <55955411+jeffxu2004@users.noreply.github.com>
2023-12-01 22:19:48 -0500

    Delete main.d

--- main.d
+++
@@ -1,5 +0,0 @@
-main.o: main.cc board.h pieces/piece.h piececreator.h pieces/blank.h \
- pieces/piece.h pieces/pawn.h pieces/rook.h pieces/knight.h \
- pieces/bishop.h pieces/queen.h pieces/king.h pieces/../observer.h \
- displays/controller.h displays/../observer.h displays/textdisplay.h \
- displays/graphicsdisplay.h displays/window.h
