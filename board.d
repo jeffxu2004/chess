f5a1b9f9f797269bec4aa7d77a9e81158cd81a22
Jeffrey Xu <55955411+jeffxu2004@users.noreply.github.com>
2023-12-01 22:19:29 -0500

    Delete board.d

--- board.d
+++
@@ -1,3 +0,0 @@
-board.o: board.cc board.h pieces/piece.h piececreator.h pieces/blank.h \
- pieces/piece.h pieces/pawn.h pieces/rook.h pieces/knight.h \
- pieces/bishop.h pieces/queen.h pieces/king.h pieces/../observer.h
