e985b1e865f6c5f5b2144b3fb80249d2f4b13818
Richard Wei <richard33113@gmail.com>
2023-12-02 21:24:38 -0500

    added chessbotcreator

--- chessbot/chessbotcreator.h
+++ chessbot/chessbotcreator.h
@@ -14,9 +14,18 @@ using namespace std;
 
 class ChessBotCreator {
 public:
-        static unique_ptr<ChessBot> make(string s) {
-            if (s == "computer1")
-            return make_unique<LevelOne>();
+        static unique_ptr<ChessBot> makeBot(string s, Colour c) {
+            if (s == "computer1" || s == "c1"){
+                return make_unique<LevelOne>(c);               
+            } else if (s == "computer2" || s == "c2"){
+                return make_unique<LevelTwo>(c);
+            } else if (s == "computer3" || s == "c3"){
+                return make_unique<LevelThree>(c);
+            } else if (s == "computer4" || s == "c4"){
+                return make_unique<LevelFour>(c);
+            } else {
+                return make_unique<LevelOne>(c);                  
+            }
         }
 };
 
