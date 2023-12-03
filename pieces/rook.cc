ae52e5bf5c1fa625dba900bbef8fa533f97b94ce
jbwang <jbwang@uwaterloo.ca>
2023-12-03 17:14:21 -0500

    fix segfault when rook move horizontally

--- pieces/rook.cc
+++ pieces/rook.cc
@@ -24,15 +24,15 @@ vector<pair<char, int>> Rook::getMoves(const Board &b) const {
 			moves.push_back(make_pair(static_cast<char>(i+'a'), coords.second));
 		} else {
 			// Check if piece is opposing colour (can be taken)
-            if (tempGrid[8 - coords.second][i]->getSide() != this->side) moves.push_back(make_pair(i, coords.second));
+            if (tempGrid[8 - coords.second][i]->getSide() != this->side) moves.push_back(make_pair(static_cast<char>(i+'a'), coords.second));
 			break;
 		}
 	}
 	for (int i = coords.first - 'a' + 1; i < tempGrid.size(); i++) {
-		if (tempGrid[coords.second - 1][i]->pieceType() == PieceType::Blank) {
-			moves.push_back(make_pair(i, 8 - coords.second));
+		if (tempGrid[8 - coords.second][i]->pieceType() == PieceType::Blank) {
+			moves.push_back(make_pair(static_cast<char>(i+'a'), coords.second));
 		} else {
-			if (tempGrid[8 - coords.second][i]->getSide() != this->side) moves.push_back(make_pair(i, coords.second));
+			if (tempGrid[8 - coords.second][i]->getSide() != this->side) moves.push_back(make_pair(static_cast<char>(i+'a'), coords.second));
 			break;
 		}
 	}
