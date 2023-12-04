e7632d5962488eeff53e05656d16f44538e86950
J-Bungus <113871762+J-Bungus@users.noreply.github.com>
2023-12-04 18:31:38 -0500

    Delete pieces/bishop.d

--- pieces/bishop.d
+++
@@ -1,8 +0,0 @@
-pieces/bishop.o: pieces/bishop.cc pieces/bishop.h pieces/piece.h \
- pieces/../enumClasses.h pieces/../board.h pieces/../pieces/piece.h \
- pieces/../piececreator.h pieces/../pieces/blank.h \
- pieces/../pieces/piece.h pieces/../pieces/pawn.h pieces/../pieces/rook.h \
- pieces/../pieces/knight.h pieces/../pieces/bishop.h \
- pieces/../pieces/queen.h pieces/../pieces/king.h \
- pieces/../pieces/../observer.h pieces/../pieces/../enumClasses.h \
- pieces/../pieces/rook.h pieces/../enumClasses.h
