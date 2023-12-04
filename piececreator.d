fb7200828d45a503808adb300fcd1733e932b2c3
J-Bungus <113871762+J-Bungus@users.noreply.github.com>
2023-12-04 18:30:36 -0500

    Delete piececreator.d

--- piececreator.d
+++
@@ -1,4 +0,0 @@
-piececreator.o: piececreator.cc piececreator.h pieces/piece.h \
- pieces/../enumClasses.h pieces/blank.h pieces/piece.h pieces/pawn.h \
- pieces/rook.h pieces/knight.h pieces/bishop.h pieces/queen.h \
- pieces/king.h pieces/../observer.h pieces/rook.h enumClasses.h
