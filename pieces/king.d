5d1da3e89f22de7c58ed8252d4b9894d2bcc3e0e
J-Bungus <113871762+J-Bungus@users.noreply.github.com>
2023-12-04 18:32:20 -0500

    Delete pieces/king.d

--- pieces/king.d
+++
@@ -1,7 +0,0 @@
-pieces/king.o: pieces/king.cc pieces/king.h pieces/piece.h \
- pieces/../enumClasses.h pieces/../observer.h pieces/../enumClasses.h \
- pieces/rook.h pieces/../board.h pieces/../pieces/piece.h \
- pieces/../piececreator.h pieces/../pieces/blank.h \
- pieces/../pieces/piece.h pieces/../pieces/pawn.h pieces/../pieces/rook.h \
- pieces/../pieces/knight.h pieces/../pieces/bishop.h \
- pieces/../pieces/queen.h pieces/../pieces/king.h
