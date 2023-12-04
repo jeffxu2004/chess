e581e56f9a067774a2fe5e8cd10396ac9c9e3514
J-Bungus <113871762+J-Bungus@users.noreply.github.com>
2023-12-04 18:34:58 -0500

    Delete displays/textdisplay.d

--- displays/textdisplay.d
+++
@@ -1,9 +0,0 @@
-displays/textdisplay.o: displays/textdisplay.cc displays/textdisplay.h \
- displays/../pieces/piece.h displays/../pieces/../enumClasses.h \
- displays/../piececreator.h displays/../pieces/piece.h \
- displays/../pieces/blank.h displays/../pieces/piece.h \
- displays/../pieces/pawn.h displays/../pieces/rook.h \
- displays/../pieces/knight.h displays/../pieces/bishop.h \
- displays/../pieces/queen.h displays/../pieces/king.h \
- displays/../pieces/../observer.h displays/../pieces/../enumClasses.h \
- displays/../pieces/rook.h displays/../enumClasses.h
